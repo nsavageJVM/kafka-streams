7e560611b87f52124381e23083ef3962651caca8
nsavagejvm <nscode1@gmail.com>
2015-07-29 20:57:09 +0300

    update

--- src/main/java/com/eduonix/kafka/ParseTwitterStreamTask.java
+++ src/main/java/com/eduonix/kafka/ParseTwitterStreamTask.java
@@ -41,7 +41,7 @@ public class ParseTwitterStreamTask implements StreamTask  {
             JSONObject jsonObj = (JSONObject) obj;
             String     text    = (String) jsonObj.get("text");
 
-            collector.send(new OutgoingMessageEnvelope(new SystemStream("kafka", "tweets"), text));
+            collector.send(new OutgoingMessageEnvelope(new SystemStream("kafka", "tweets-parsed"), text));
         } catch (ParseException pe) {}
 
 
